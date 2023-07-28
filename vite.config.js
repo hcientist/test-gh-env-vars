56513a4b0fbf034c49e279dc70976ea14fba6385
Michael Stewart <tgm@vt.edu>
2023-07-28 14:26:53 -0400

    attempt to use env vars for github actions deployments

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 
+const PRAXLY_PATH = process.env.PRAXLY_PATH || "/";
+console.log("\n\n\nPRAXLY_PATH", PRAXLY_PATH, '\n\n\n');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/test-gh-env-vars",
+  base: PRAXLY_PATH,
   build: {
     outDir: "dist",
   },
